d44f323d1d4e55063434697450e798db1e82f593
guomingshun <1449863840@qq.com>
2023-06-20 09:02:58 +0800

    1

---
+++ learncode/Model/UnionFind.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace learncode.Model
+{
+    public class UnionFind
+    {
+        private List<int> parent;
+
+
+        public void Init(int n)
+        {
+            for(int i=0;i<n;i++)
+            {
+                parent[i] = i;
+            }
+        }
+        public int Find(int x)
+        {
+            if (parent[x] == x)
+                return x;
+            else
+            {
+                parent[x] = Find(parent[x]);
+                return parent[x];
+            }
+        }
+        public void merge(int i,int j)
+        {
+            parent[Find(j)] = Find(i);
+        }
+    }
+}
