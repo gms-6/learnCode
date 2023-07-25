c8742f49409f4e9241bd2549b8e645060fd5f9df
guomingshun <1449863840@qq.com>
2023-07-25 13:54:37 +0800

    1

--- learncode/LinqModel/LinqTest.cs
+++ learncode/LinqModel/LinqTest.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using learncode.LinqModel.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,11 @@ namespace learncode.LinqModel
 {
     public class LinqTest
     {
+        IList<Racer> racer=new List<Racer>();
+        public IEnumerable<bool> GetRacers()
+        {
+            var res = racer.Where(r => r.FirstName == "N").Where(r => r.Wins == 1).Select(r=>r.Starts==2);
+            return res;
+        }
     }
 }
