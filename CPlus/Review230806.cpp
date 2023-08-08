0ac27d5bce0b436ffa2a0a861b097e33ef5b2d7f
guomingshun <1449863840@qq.com>
2023-08-08 12:08:09 +0800

    1

--- CPlus/Review230806.cpp
+++ CPlus/Review230806.cpp
@@ -1,32 +1,33 @@
 #include <vector>
 #include<algorithm>
 #include <iostream>
+#include "TreeNode.cpp"
 
 using namespace std;
 
 class Review230806
 {
-	
+
 public:
 	vector<vector<int>> premute(vector<int>& nums)
 	{
-			int n = nums.size();
-			vector<vector<int>> res;
-			vector<bool> status;
-			vector<int> output;
-			status.assign(n,false);
-			backtrack(res, output, status, nums);
-			for (auto tmp1 : res)
+		int n = nums.size();
+		vector<vector<int>> res;
+		vector<bool> status;
+		vector<int> output;
+		status.assign(n, false);
+		backtrack(res, output, status, nums);
+		for (auto tmp1 : res)
+		{
+			for (auto tmp2 : tmp1)
 			{
-				for (auto tmp2 : tmp1)
-				{
-					cout << tmp2 << " ";
-				}
-				cout << endl;
+				cout << tmp2 << " ";
 			}
-			return res;
+			cout << endl;
+		}
+		return res;
 	}
-	void backtrack(vector<vector<int>>& res,vector<int>&output,vector<bool>& status,vector<int>& nums)
+	void backtrack(vector<vector<int>>& res, vector<int>& output, vector<bool>& status, vector<int>& nums)
 	{
 		if (output.size() == nums.size())
 		{
@@ -39,10 +40,57 @@ public:
 			{
 				status[i] = true;
 				output.push_back(nums[i]);
-				backtrack(res,output,status,nums);
+				backtrack(res, output, status, nums);
 				output.pop_back();
 				status[i] = false;;
 			}
 		}
 	}
+
+	vector<int> PreOrderTraversal(TreeNode* root)
+	{
+		vector<int> list;
+		PreOrderTraversalDFS(root, list);
+		return list;
+	}
+	void PreOrderTraversalDFS(TreeNode* root, vector<int>& list)
+	{
+		if (root == nullptr)
+			return;
+		list.push_back(root->val);
+		PreOrderTraversalDFS(root->left,list);
+		PreOrderTraversalDFS(root->right,list);
+	}
+
+	vector<int> InOrderTraversal(TreeNode* root)
+	{
+		vector<int> list;
+		InOrderTraversalDFS(root,list);
+		return list;
+	}
+	void InOrderTraversalDFS(TreeNode* root,vector<int>& list)
+	{
+		if (root == nullptr)
+			return;
+		InOrderTraversalDFS(root->left,list);
+		list.push_back(root->val);
+		InOrderTraversalDFS(root->right,list);
+	}
+
+	
+	vector<int> PostOrderTraversal(TreeNode* root)
+	{
+		vector<int> list;
+		PostOrderTraversalDFS(root,list);
+		return list;
+	}
+	void PostOrderTraversalDFS(TreeNode* root,vector<int>& list)
+	{
+		if (root == nullptr)
+			return;
+		PostOrderTraversalDFS(root->left,list);
+		PostOrderTraversalDFS(root->right,list);
+		list.push_back(root->val);
+	}
+
 };
