b361ad980074c07f451e7d1661df819564e96854
guomingshun <1449863840@qq.com>
2023-08-22 15:55:35 +0800

    1

--- learncode/SqlCode/sqlCodeTest.sql
+++ learncode/SqlCode/sqlCodeTest.sql
@@ -1,12 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace learncode.SqlCode
-{
-    internal class sqlCodeTest
-    {
-    }
-}
+﻿//多表查询 leetcode175
+//"LEFT JOIN" 是 SQL 中的一种连接操作，用于将两个或多个表中的数据合并，返回左侧表中的所有行，以及与右侧表匹配的行。如果右侧表中没有匹配的行，则会用 NULL 填充。
+select firstName,secondName,city,state
+from Person left join Address
+on Person.PersonId=Address.PersonId;
